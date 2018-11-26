d976b5d9c312c7fe05d9477c4314334b935b64f4
Adhi Ravishankar <adhiravishankar@gmail.com>
2018-11-26 11:31:08 -0500

    finish listings

--- app/src/main/java/edu/gatech/ecotourism/activities/ExperienceActivity.java
+++ app/src/main/java/edu/gatech/ecotourism/activities/ExperienceActivity.java
@@ -1,6 +1,8 @@
 package edu.gatech.ecotourism.activities;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import edu.gatech.ecotourism.Listing;
@@ -15,6 +17,14 @@ public class ExperienceActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experience);
 
+        listing = getIntent().getParcelableExtra("listing");
+        TextView type = findViewById(R.id.type);
+        type.setText(listing.getType());
 
+        TextView title = findViewById(R.id.title);
+        title.setText(listing.getTitle());
+
+        ImageView backdrop = findViewById(R.id.backdrop);
+        backdrop.setImageResource(listing.getImage());
     }
 }
--- app/src/main/java/edu/gatech/ecotourism/activities/HouseActivity.java
+++ app/src/main/java/edu/gatech/ecotourism/activities/HouseActivity.java
@@ -2,6 +2,8 @@ package edu.gatech.ecotourism.activities;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import edu.gatech.ecotourism.Listing;
 import edu.gatech.ecotourism.R;
@@ -16,5 +18,12 @@ public class HouseActivity extends Activity {
         setContentView(R.layout.activity_house);
 
         listing = getIntent().getParcelableExtra("listing");
-    }
+        TextView type = findViewById(R.id.type);
+        type.setText(listing.getType());
+
+        TextView title = findViewById(R.id.title);
+        title.setText(listing.getTitle());
+
+        ImageView backdrop = findViewById(R.id.backdrop);
+        backdrop.setImageResource(listing.getImage());    }
 }
